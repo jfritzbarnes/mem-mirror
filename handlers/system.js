ad470a13dbf0a41f7ef3136c7464bb94edf7d150
J. Fritz Barnes <fritz@stonevalleypartners.com>
2016-11-09 14:58:57 -0800

    start /system APIs

---
+++ handlers/system.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Dropbox = require('dropbox');
+
+exports.dbInit = function(request, reply) {
+  if(req.server.app.ready) {
+    return reply({status: 'fail', message: 'server is in ready state'});
+  }
+
+  const token = req.params.token;
+  var dbx = new Dropbox({accessToken: token});
+  dbx.filesListFolder({path: ''})
+    .then((list) => {
+      return reply({listFiles: list});
+    })
+    .catch((err) => {
+      console.log(err);
+      return reply({status: 'fail', message: err.message});
+    });
+};
---
+++ routes/system.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const System = require('../handlers/system.js');
+
+const API_BASE_PATH = '/system';
+
+const routes = [];
+
+routes.push({
+  method: 'GET',
+  path: API_BASE_PATH,
+  handler: (req, reply) => { reply({db: {ready: req.server.app.ready}}); }
+});
+
+routes.push({
+  method: '',
+  path: `${API_BASE_PATH}/dbinit/{token}`,
+  handler: System.dbInit
+});
+
+module.exports = routes;
